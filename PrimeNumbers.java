748fa3861fef94acf4b56bbd44366ea24feaf757
amrutha-nambiar <153012612+amrutha-nambiar@users.noreply.github.com>
2024-08-27 22:29:28 +0530

    Update PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,45 +1,53 @@
-public class PrimeNumbers {
+//Write a Java program to find the prime numbers from 1 to n, where n is passed using the command-line argument (CLA).
 
-    // Method to check if a number is prime
-    private static boolean isPrime(int num) {
-        if (num <= 1) {
+public class PrimeNumbers
+{
+
+    private static boolean isPrime(int num)
+    {
+        if (num <= 1)
+        {
             return false;
         }
-        if (num == 2) {
+        if (num == 2)
+        {
             return true;
         }
-        if (num % 2 == 0) {
+        if (num % 2 == 0)
+        {
             return false;
         }
-        for (int i = 3; i * i <= num; i += 2) {
-            if (num % i == 0) {
+        for (int i = 3; i * i <= num; i += 2)
+        {
+            if (num % i == 0)
+            {
                 return false;
             }
         }
         return true;
     }
 
-    // Method to print all prime numbers up to n
-    private static void printPrimesUpTo(int n) {
-        for (int i = 1; i <= n; i++) {
-            if (isPrime(i)) {
+    private static void printPrimesUpTo(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (isPrime(i))
+            {
                 System.out.print(i + " ");
             }
         }
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        // Ensure a command-line argument is provided
-        if (args.length != 1) {
+    public static void main(String[] args)
+    {
+        if (args.length != 1)
+        {
             System.out.println("Please provide a single positive integer as a command-line argument.");
             return;
         }
 
-        // Convert the command-line argument to an integer
         int n = Integer.parseInt(args[0]);
-
-        // Print all prime numbers up to n
         printPrimesUpTo(n);
     }
 }
