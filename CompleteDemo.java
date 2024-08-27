c530f72f6c68ac9ac503d3ecc632621640efa120
amrutha-nambiar <153012612+amrutha-nambiar@users.noreply.github.com>
2024-08-27 21:58:07 +0530

    Update CompleteDemo.java

--- CompleteDemo.java
+++ CompleteDemo.java
@@ -1,79 +1,88 @@
-// Class Complete1 containing instance variables of String and byte type
-class Complete1 {
-    // Instance variables
+//Define the following classes:
+
+    //- Class Complete-1: Contains instance variables of string and integer type.
+    //Class Complete-2: Contains:
+    //- A Power method to find the square and cube of an integer type number.
+    //- An Add method to find the sum of two integer numbers.
+    //- Class Complete-3: Contains:
+   // - Instance variables of integer type x and y.
+    //- A method to display the values of x and y.
+    //- A Subtract method to take the difference of x and y.
+
+
+
+class Complete1 
+{
     String text;
     byte number;
 
-    // Constructor to initialize instance variables
-    public Complete1(String textValue, byte numberValue) {
+    public Complete1(String textValue, byte numberValue) 
+    {
         text = textValue;
         number = numberValue;
     }
 
-    // Method to display instance variables
-    public void display() {
+    public void display() 
+    {
         System.out.println("String value: " + text);
         System.out.println("Byte value: " + number);
     }
 }
 
-// Class Complete2 containing methods to find the square and cube of a number, and to add two numbers
-class Complete2 {
-    // Method to find the square of an integer
-    public int power(int number) {
+class Complete2 
+{
+    public int power(int number) 
+    {
         return number * number;
     }
 
-    // Method to find the cube of an integer
-    public int cube(int number) {
+    public int cube(int number) 
+    {
         return number * number * number;
     }
 
-    // Method to add two integers
-    public int add(int num1, int num2) {
+    public int add(int num1, int num2) 
+    {
         return num1 + num2;
     }
 }
 
-// Class Complete3 containing instance variables x and y of integer type, and methods to display and subtract
-class Complete3 {
-    // Instance variables
+class Complete3 
+{
     int x;
     int y;
 
-    // Constructor to initialize instance variables
-    public Complete3(int xValue, int yValue) {
+    public Complete3(int xValue, int yValue) 
+    {
         x = xValue;
         y = yValue;
     }
 
-    // Method to display the values of x and y
-    public void display() {
+    public void display() 
+    {
         System.out.println("Value of x: " + x);
         System.out.println("Value of y: " + y);
     }
 
-    // Method to subtract y from x
-    public int subtract() {
+    public int subtract() 
+    {
         return x - y;
     }
 }
 
-// Main class CompleteDemo to test the functionality of the above classes
-public class CompleteDemo {
-    public static void main(String[] args) {
-        // Creating an object of Complete1
+public class CompleteDemo 
+{
+    public static void main(String[] args) 
+    {
         Complete1 complete1 = new Complete1("Hello", (byte) 25);
         complete1.display();
 
-        // Creating an object of Complete2
         Complete2 complete2 = new Complete2();
         int number = 5;
         System.out.println("Square of " + number + ": " + complete2.power(number));
         System.out.println("Cube of " + number + ": " + complete2.cube(number));
         System.out.println("Sum of 10 and 15: " + complete2.add(10, 15));
 
-        // Creating an object of Complete3
         Complete3 complete3 = new Complete3(20, 10);
         complete3.display();
         System.out.println("Difference between x and y: " + complete3.subtract());
