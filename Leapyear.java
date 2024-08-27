c4ddac64b124041a33cd177501035175a8e68179
amrutha-nambiar <153012612+amrutha-nambiar@users.noreply.github.com>
2024-08-27 22:53:59 +0530

    Create Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,50 @@
+/*Define a class called A containg an instance variable y which denotes a year and and define a constant to initialise the instance  variable and 
+  method to check  whether it is a leap year or not.*/
+
+import java.util.Scanner;
+
+class A
+{
+    private int y;
+
+    public A(int year)
+    {
+        this.y = year;
+    }
+
+    public boolean isLeapYear()
+    {
+        if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+public class B
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int yearInput = scanner.nextInt();
+
+        A year = new A(yearInput);
+
+        if (year.isLeapYear())
+        {
+            System.out.println(yearInput + " is a leap year.");
+        }
+        else
+        {
+            System.out.println(yearInput + " is not a leap year.");
+        }
+
+        scanner.close();
+    }
+}
