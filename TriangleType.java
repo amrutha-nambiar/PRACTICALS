239e621e5183faaf8c9c8669a93380c5c82d42c3
amrutha-nambiar <153012612+amrutha-nambiar@users.noreply.github.com>
2024-08-27 22:35:25 +0530

    Update TriangleType.java

--- TriangleType.java
+++ TriangleType.java
@@ -1,66 +1,78 @@
-public class TriangleType {
+/*Write a Java program to determine whether three given positive integer (a, b, and c) can form the sides of a triangle. 
+    If yes,determine the type of triangle (right-angled, acute, or obtuse).*/
 
-    // Static inner class for triangle-related calculations
-    private static class TriangleUtils {
+public class TriangleType
+{
 
-        // Method to check if the sides can form a triangle
-        private static boolean isValidTriangle(int a, int b, int c) {
+    private static class TriangleUtils
+    {
+
+        private static boolean isValidTriangle(int a, int b, int c)
+        {
             return (a + b > c) && (a + c > b) && (b + c > a);
         }
 
-        // Method to determine the type of triangle
-        private static String determineTriangleType(int a, int b, int c) {
+        private static String determineTriangleType(int a, int b, int c)
+        {
             int x = a, y = b, z = c;
 
-            // Find the longest side
-            if (a >= b && a >= c) {
+            if (a >= b && a >= c)
+            {
                 x = b;
                 y = c;
                 z = a;
-            } else if (b >= a && b >= c) {
+            }
+            else if (b >= a && b >= c)
+            {
                 x = a;
                 y = c;
                 z = b;
-            } else {
+            }
+            else
+            {
                 x = a;
                 y = b;
                 z = c;
             }
 
-            // Calculate squares of the sides
             int x2 = x * x;
             int y2 = y * y;
             int z2 = z * z;
 
-            // Determine the type of triangle
-            if (x2 + y2 == z2) {
+            if (x2 + y2 == z2)
+            {
                 return "right";
-            } else if (x2 + y2 < z2) {
+            }
+            else if (x2 + y2 < z2)
+            {
                 return "obtuse";
-            } else {
+            }
+            else
+            {
                 return "acute";
             }
         }
     }
 
-    public static void main(String[] args) {
-        // Ensure at least three command-line arguments are provided
-        if (args.length < 3) {
+    public static void main(String[] args)
+    {
+        if (args.length < 3)
+        {
             System.out.println("Please provide three positive integers as command-line arguments.");
             return;
         }
 
-        // Convert command-line arguments to integers
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
 
-        // Check if the sides can form a triangle
-        if (TriangleUtils.isValidTriangle(a, b, c)) {
-            // Determine the type of triangle
+        if (TriangleUtils.isValidTriangle(a, b, c))
+        {
             String triangleType = TriangleUtils.determineTriangleType(a, b, c);
             System.out.println("The sides form a " + triangleType + " triangle.");
-        } else {
+        }
+        else
+        {
             System.out.println("The sides do not form a valid triangle.");
         }
     }
